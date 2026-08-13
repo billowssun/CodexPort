2e1d2d81a6d32ec035f22e52183874465ba4072e
billowssun <billoowssun@outlook.com>
2026-08-13 12:18:16 +0800

    Position CodexPort as a multi-device migration tool

--- website/api/poster.js
+++ website/api/poster.js
@@ -1,4 +1,4 @@
-const POSTER_URL = 'https://raw.githubusercontent.com/billowssun/CodexPort/main/website/public/codexport-v1.2-poster.png?revision=github-icon';
+const POSTER_URL = 'https://raw.githubusercontent.com/billowssun/CodexPort/main/website/public/codexport-v1.2-poster.png?revision=multi-device';
 
 module.exports = async function poster(request, response) {
   const source = await fetch(POSTER_URL);
